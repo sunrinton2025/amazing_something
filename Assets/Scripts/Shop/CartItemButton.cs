4f016e98b1bf645e974061ca8c7303cf495883da
BshAIdev <96278204+BshAIdev@users.noreply.github.com>
2025-07-18 23:13:06 +0900

    UI

---
+++ Assets/Scripts/Shop/CartItemButton.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CartItemButton : MonoBehaviour
+{
+    private ShopItem item;
+    private ShopCartManager cartManager;
+
+    public void Init(ShopItem newItem, ShopCartManager manager)
+    {
+        item = newItem;
+        cartManager = manager;
+        GetComponentInChildren<TextMeshProUGUI>().text = item.itemName + " (" + item.price + "원)";
+        GetComponent<Button>().onClick.AddListener(OnRemove);
+    }
+
+    void OnRemove()
+    {
+        cartManager.RemoveFromCart(item, gameObject);
+    }
+}
